ef5836a3ec9d6ac43e7cabb0b6e9dea95a89ee2d
tinnamchoi <tinnam.choi@gmail.com>
2022-10-04 16:42:39 +1030

    Improved menu output style

--- game/Menu.cpp
+++ game/Menu.cpp
@@ -18,9 +18,30 @@ int Menu::getNumOptions() { return numOptions; }
 void Menu::setTitle(std::string title) { this->title = title; }
 void Menu::setOption(int index, std::string option) { options[index] = option; }
 void Menu::setNumOptions(int numOptions) { this->numOptions = numOptions; }
+
+/* Menu format 
+
+== Title ==
+-----------------------------
+| 1. Option 1 | 2. Option 2 |
+| 3. Option 3 | 4. Option 4 |
+-----------------------------
+
+*/
+
 void Menu::printMenu() {
-  std::cout << title << std::endl;
+  cout << "== " << title << " ==" << endl;
+  cout << "-----------------------------" << endl;
   for (int i = 0; i < numOptions; i++) {
-    std::cout << i + 1 << ". " << options[i] << std::endl;
+    if (i % 2 == 0) {
+      cout << "| ";
+    }
+    cout << i + 1 << ". " << options[i];
+    if (i % 2 == 1) {
+      cout << " |" << endl;
+    } else {
+      cout << " | ";
+    }
   }
+  cout << "-----------------------------" << endl;
 }
