2c3a7155134f00b1a4042f6b8d7addfc36fe5e83
sarahmccauley <101790383+sarahmccauley@users.noreply.github.com>
2022-10-15 23:11:00 +1030

    adding if statements to determine scoring
    will need to implement type of pokemon and move hp

--- game/Player.cpp
+++ game/Player.cpp
@@ -9,15 +9,51 @@ Player::Player() {
     pokemon[5] = "Pokemon 6";
 
     recentMove = 'a'; //currently define 'a' for attack
+    score = 0;
 }
 
 char Player::getMove() { //return the move made
     return recentMove;
 }
 
-bool Player::win(Player * opponent) { //polymorphism
+int Player::win(Player * opponent) { //polymorphism
     char recent = recentMove; //take the move
     char op = opponent->getMove();
 
-    //create an if statement to determine scores based on move made - return true if player wins, false if comp wins
+    //create an if statement to determine scores based on move made - return the score
+    //score is calculated based on type and chosen move
+    if (recent == 'a') { //if user attacks
+        if (op == 'a') { //if cmp also attacks
+            //calculate difference in attack hp
+        } 
+        else if (op == 'd') { //if cmp defends
+            //calculate difference in attack and defense hp
+        } 
+        else { //cmp special attack
+            //calculate difference in attack and special attack hp
+        }
+    }
+
+    else if (recent == 'd') { //if user defends
+        if (op == 'a') {
+            //calculate difference in attack hp
+        } 
+        else if (op == 'd') { //cmp defends
+            //calculate difference in attack and defense hp
+        }
+        else { //cmp special attack
+            //calculate difference in attack and special attack hp
+        }
+    }
+
+    else { //user special attacks
+    if (op == 'a') {
+        //calculate difference in attack hp
+    }
+    else if (op == 'd') { //cmp defends
+        //calculate difference in attack and defense hp
+    }
+    else { //op special attacks
+        //calculate difference in attack and special attack hp
+    }
 }
