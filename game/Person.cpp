55ba364d2e5f4890ba88f8991bdb7fcbdf9945a9
sarahmccauley <101790383+sarahmccauley@users.noreply.github.com>
2022-10-16 21:57:53 +1030

    Update Person.cpp

--- game/Person.cpp
+++ game/Person.cpp
@@ -4,18 +4,18 @@
 #include <string.h>
 
 Person::Person() {
-    name = "Player 1";
+    name = "Player 1";  //default name of player
 }
 
 void Person::setName(std::string name) {
-    this->name = name;
+    this->name = name; //set name
 }
 
 std::string Person::getName() {
-    return name;
+    return name; //get name
 }
 
 void Person::move() {
     cout << "Enter move (): " << endl; //enter a character to represent a move
-    cin >> move;
+    cin >> move; //take move and store into the void move variable from player main class
 }
