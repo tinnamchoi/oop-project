03f3b7af50e927ccd64efe796f680885e06f41b3
sarahmccauley <101790383+sarahmccauley@users.noreply.github.com>
2022-10-16 21:22:21 +1030

    adding move to child class

--- game/Person.cpp
+++ game/Person.cpp
@@ -1,11 +1,18 @@
 #include "Person.h"
 
 Person::Person() {
-  name = "Player 1";
+    name = "Player 1";
 }
+
 void Person::setName(std::string name) {
-  this->name = name;
+    this->name = name;
 }
+
 std::string Person::getName() {
-  return name;
+    return name;
+}
+
+void Person::move() {
+    std::cout << "Enter move (): " << std::endl; //enter a character to represent a move
+    cin >> move;
 }
