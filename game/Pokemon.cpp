57bb2cf8fd3cdd2ec680204fb92ce42950c8ff9d
tinnamchoi <tinnam.choi@gmail.com>
2022-10-16 16:49:50 +1030

    Added more values to the Pokemon class

--- game/Pokemon.cpp
+++ game/Pokemon.cpp
@@ -4,30 +4,49 @@
 
 Pokemon::Pokemon() {}
 void Pokemon::newPokemon(int id) {
+  level = 1;
   switch (id) {
     case 0:
-      name = "Bulbasaur";
-      attack = 49;
+      name = "Pokemon 1";
+      attack  = 2;
+      special = 3;
+      health  = 3;
+      defense = 4;
       break;
     case 1:
-      name = "Charmander";
-      attack = 52;
+      name = "Pokemon 2";
+      attack  = 2;
+      special = 4;
+      health  = 5;
+      defense = 1;
       break;
     case 2:
-      name = "Squirtle";
-      attack = 48;
+      name = "Pokemon 3";
+      attack  = 3;
+      special = 4;
+      health  = 2;
+      defense = 3;
       break;
     case 3:
-      name = "Pikachu";
-      attack = 55;
+      name = "Pokemon 4";
+      attack  = 5;
+      special = 1;
+      health  = 3;
+      defense = 3;
       break;
     case 4:
-      name = "Eevee";
-      attack = 55;
+      name = "Pokemon 5";
+      attack  = 2;
+      special = 2;
+      health  = 3;
+      defense = 5;
       break;
     case 5:
-      name = "Mewtwo";
-      attack = 99;
+      name = "Pokemon 6";
+      attack  = 2;
+      special = 4;
+      health  = 4;
+      defense = 2;
       break;
   }
 }
