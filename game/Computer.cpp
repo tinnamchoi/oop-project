ada9d2fcf99a7fda9066c6de2cace92c8baa3c4f
sarahmccauley <101790383+sarahmccauley@users.noreply.github.com>
2022-10-16 22:47:19 +1030

    Update Computer.cpp

--- game/Computer.cpp
+++ game/Computer.cpp
@@ -2,6 +2,7 @@
 #include "Computer.h"
 
 #include <time.h>
+#include <string.h>
 
 Computer::Computer() {
     //not sure if there is anything to include in here
@@ -13,16 +14,16 @@ void Computer::move() {
                                    //there are 3 moves as of current - attack, defence, special attack
     switch(random) {
         case 1:
-            move = 'a'; //a for attack
+            recentMove = 'a'; //a for attack
             break;
         case 2:
-            move = 'd'; //d for defence
+            recentMove = 'd'; //d for defence
             break;
         case 3: 
-            move = 's'; //s for special attack
+            recentMove = 's'; //s for special attack
             break;
         default:
-            move = 'a'; //make attack the default move
+            recentMove = 'a'; //make attack the default move
             break;
     }
 }
