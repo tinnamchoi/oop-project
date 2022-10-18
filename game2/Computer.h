fa42a28e390fe4dc994c1a508ef738cd95be93a6
tinnamchoi <tinnam.choi@gmail.com>
2022-10-18 21:04:38 +1030

    Computer name changes merge

--- game2/Computer.h
+++ game2/Computer.h
@@ -7,9 +7,10 @@ class Computer : public Player {
  public:
   Computer(int landmark);
   int currentPokemon = 0;
-  std::string names[10] = {
-      "Computer 1", "Computer 2", "Computer 3", "Computer 4", "Computer 5",
-      "Computer 6", "Computer 7", "Computer 8", "Computer 9", "Computer 10"};
+  std::string names[10] = {"Enemy Bryan",  "Enemy Gregory", "Enemy Cassie",
+                           "Enemy Peter",  "Enemy Rowley",  "Enemy Regina",
+                           "Enemy Steven", "Enemy Darcy",   "Enemy Sarah",
+                           "Enemy Timothy"};
 };
 
 #endif
